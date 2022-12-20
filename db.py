ea2f493202c62098a96fabc99dcd2c8363b57390
aclozovoy <aclozovoy@gmail.com>
2022-12-19 17:43:56 -0800

    Add printouts table

--- db.py
+++ db.py
@@ -55,33 +55,33 @@ def db_conn():
     cursor.fetchall()
 
     # CREATE PRINTOUTS TABLE
-    # sql = '''
-    # CREATE TABLE IF NOT EXISTS printouts (
-    # id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    # timestamp TIMESTAMP DEFAULT NOW(),
-    # ip_address VARCHAR(100) NOT NULL,
-    # Sds DECIMAL(7,3),
-    # Wp DECIMAL(7,3),
-    # units VARCHAR(10),
-    # R DECIMAL(7,3),
-    # Omega0
-    # R_mu
-    # z
-    # h
-    # Ta
-    # Hf
-    # Ip
-    # Car
-    # Rpo
-    # Omegaop
-    # ComponentNumber INT,
-    # ComponentType VARCHAR(250),
-    # Fp DECIMAL(9,3),
-    # OopFp DECIMAL(9,3)
-    # )
-    # '''
-    # cursor.execute(sql)
-    # cursor.fetchall()
+    sql = '''
+    CREATE TABLE IF NOT EXISTS printouts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT NOW(),
+    ip_address VARCHAR(100) NOT NULL,
+    Sds DECIMAL(7,3),
+    Wp DECIMAL(7,3),
+    units VARCHAR(10),
+    R DECIMAL(7,3),
+    Omega0 DECIMAL(2,1),
+    R_mu DECIMAL(2,1),
+    z DECIMAL(9,3),
+    h DECIMAL(9,3),
+    Ta DECIMAL(9,3),
+    Hf DECIMAL(9,3),
+    Ip DECIMAL(2,1),
+    Car DECIMAL(2,1),
+    Rpo DECIMAL(2,1),
+    Omegaop DECIMAL(2,1),
+    ComponentNumber INT,
+    ComponentType VARCHAR(250),
+    Fp DECIMAL(9,3),
+    OopFp DECIMAL(9,3)
+    )
+    '''
+    cursor.execute(sql)
+    cursor.fetchall()
 
 
 
@@ -98,7 +98,7 @@ def db_pages(page):
     from flask import request
 
     cursor = db_conn()
-    # ip_addr = request.remote_addr
+
     ip_addr = request.access_route[-1]
 
     sql = f'''
@@ -119,10 +119,10 @@ def db_printout(Sds, Wp, units, R, Omega0, R_mu, z, h, Ta, Hf, Ip, Car, Rpo, Ome
 
     cursor = db_conn()
 
-    ip_addr = request.remote_addr
+    ip_addr = request.access_route[-1]
 
     sql = f'''
-    INSERT INTO pageviews (ip_address, Sds, Wp, units, R, Omega0, R_mu, z, h, Ta, Hf, Ip, Car, Rpo, Omegaop, ComponentNumber, ComponentType, Fp, OopFp)
+    INSERT INTO printouts (ip_address, Sds, Wp, units, R, Omega0, R_mu, z, h, Ta, Hf, Ip, Car, Rpo, Omegaop, ComponentNumber, ComponentType, Fp, OopFp)
     VALUES ('{ip_addr}', '{Sds}', '{Wp}', '{units}', '{R}', '{Omega0}', '{R_mu}', '{z}', '{h}', '{Ta}', '{Hf}', '{Ip}', '{Car}', '{Rpo}', '{Omegaop}', '{CompNum}', '{CompType}', '{Fp}', '{OopFp}');
     '''
 
